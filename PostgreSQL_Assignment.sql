8e432d515767a8655fbdbaa4e05dfb54bb05368e
showrov4g <showrovg00@gmail.com>
2025-05-25 22:46:35 +0600

    problem four solve

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -5,44 +5,90 @@ CREATE TABLE rangers (
     region VARCHAR(100) NOT NULL
 );
 
-
- CREATE TABLE species (
+CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(100) NOT NULL,
     scientific_name VARCHAR(150) NOT NULL,
     discovery_date DATE NOT NULL,
     conservation_status VARCHAR(50) NOT NULL
 );
- CREATE TABLE sightings (
+
+CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    species_id INT REFERENCES species(species_id),
-    ranger_id INT REFERENCES rangers(ranger_id),
+    species_id INT REFERENCES species (species_id),
+    ranger_id INT REFERENCES rangers (ranger_id),
     location VARCHAR(100) NOT NULL,
     sighting_time TIMESTAMP NOT NULL,
     notes TEXT
 );
 
-INSERT INTO rangers (ranger_id, name, region) VALUES
-(1, 'Alice Green', 'Northern Hills'),
-(2, 'Bob White', 'River Delta'),
-(3, 'Carol King', 'Mountain Range');
+INSERT INTO
+    rangers (ranger_id, name, region)
+VALUES (
+        1,
+        'Alice Green',
+        'Northern Hills'
+    ),
+    (2, 'Bob White', 'River Delta'),
+    (
+        3,
+        'Carol King',
+        'Mountain Range'
+    );
 
 --- -----
-INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
-(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
-(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
-(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
-(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+INSERT INTO
+    species (
+        species_id,
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        1,
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        2,
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        3,
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        4,
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
 
--- Register New ranger 
-INSERT INTO rangers (name, region) 
-    VALUES ('Derek Fox', 'Coastal Plains');
+-- Register New ranger
+INSERT INTO
+    rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
 
 -- ------------- 2
 SELECT COUNT(DISTINCT species_id) AS unique_species_count
-    FROM sightings;
-
+FROM sightings;
 
 -- -------- 3
-SELECT * FROM sightings
-    WHERE location ILIKE '%Pass%';
+SELECT * FROM sightings WHERE location ILIKE '%Pass%';
+
+-- ----------- 4
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+    LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY
+    r.name;
