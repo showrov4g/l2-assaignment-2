b6714a7fa73e3ccbf27164e54e289854b723eb1c
showrov4g <showrovg00@gmail.com>
2025-05-25 22:26:06 +0600

    species table create

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -4,3 +4,12 @@ CREATE TABLE rangers (
     name VARCHAR(100) NOT NULL,
     region VARCHAR(100) NOT NULL
 );
+
+
+ CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
